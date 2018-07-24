dd08bf198ecd08a5c0a93e9d03c638e1e60a7d7b
Cosmin <cosmin.serban@inflop.ro>
2018-07-24 10:31:38 +0300

    first commit

---
+++ Assets/Scripts/Active_card_script.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Active_card_script : MonoBehaviour {
+
+    private Animator animator;
+    private int state = 0;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        state = 0;
+        animator.Play("Card_1_idle_deck");
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            switch (state) {
+                case 0:
+                    state++;
+                    animator.Play("Card_1_draw");
+                    animator.
+                    break;
+                case 1:
+                    state++;
+                    animator.Play("Card_1_idle_table");
+                    animator.StopPlayback();
+                    break;
+                case 2:
+                    state++;
+                    animator.Play("Card_1_discard");
+                    animator.StopPlayback();
+                    break;
+                case 3:
+                    state = 0;
+                    animator.Play("Card_1_idle_deck");
+                    animator.StopPlayback();
+                    break;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Olaf-the-Mighty

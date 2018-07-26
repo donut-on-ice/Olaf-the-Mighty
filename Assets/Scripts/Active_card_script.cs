efd1d6a9d934651ff4ec33a422e0096f86651c66
The-Dunadan <stefan.vodita@gmail.com>
2018-07-26 22:29:45 +0300

    Animatie functionala

--- Assets/Scripts/Active_card_script.cs
+++ Assets/Scripts/Active_card_script.cs
@@ -2,44 +2,88 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Active_card_script : MonoBehaviour {
+public class Active_card_script : MonoBehaviour
+{
 
     private Animator animator;
+    //private Animation anim;
     private int state = 0;
+    public bool paused;
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        state = 0;
+        //anim = GetComponent<Animation>();
         animator.Play("Card_1_idle_deck");
+        //anim.Play("Card_1_idle_deck");
+
+        state = 0;
+        paused = true;
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            switch (state) {
-                case 0:
-                    state++;
+        switch (state)
+        {
+            case 0:
+                if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Card_1_draw"))
+                {
+                    if(paused) Pause();
                     animator.Play("Card_1_draw");
-                    animator.
-                    break;
-                case 1:
-                    state++;
+                }
+		        ++state;
+                break;
+
+            case 1:
+                if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Card_1_draw"))
+                {
                     animator.Play("Card_1_idle_table");
-                    animator.StopPlayback();
-                    break;
-                case 2:
-                    state++;
-                    animator.Play("Card_1_discard");
-                    animator.StopPlayback();
-                    break;
-                case 3:
-                    state = 0;
-                    animator.Play("Card_1_idle_deck");
-                    animator.StopPlayback();
-                    break;
-            }
+                    if (!paused) Pause();
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        //Pause();		// unpauses
+                        ++state;
+                    }
+                }
+                break;
+
+            case 2:
+				if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Card_1_discard"))
+				{
+					if(paused) Pause();
+					animator.Play("Card_1_discard");
+				}
+				++state;
+				break;
+
+            case 3:
+				if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Card_1_discard"))
+				{
+					animator.Play("Card_1_idle_deck");
+					if (!paused) Pause();
+					if (Input.GetKeyDown(KeyCode.Space))
+					{
+						//Pause();		// unpauses
+						state = 0;
+					}
+				}
+				break;
         }
     }
+    
+
+    public void Pause()
+    {
+        paused = !paused;
+        if (paused)
+        {
+            Time.timeScale = 0;
+        }
+        else if (!paused)
+        {
+            Time.timeScale = 1;
+        }
+    }
+
 }
